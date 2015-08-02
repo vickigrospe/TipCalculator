6f1409bf8467574d5bfea026cab555d10bbcbd79
Vicki Chun <vickichun@vickis-mbp.attlocal.net>
2015-08-01 22:08:50 -0700

    Initial Commit

---
+++ MyTipster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTipster
+//
+//  Created by Vicki Chun on 8/1/15.
+//  Copyright (c) 2015 Vicki Grospe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
