e851e24bb08d57b5a799b2d383688d152fdc6612
Vicki Chun <vickichun@Vickis-MacBook-Pro.local>
2015-08-02 17:03:29 -0700

    Settings view controller initialize

---
+++ MyTipster/SettingsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SettingsViewController.swift
+//  MyTipster
+//
+//  Created by Vicki Chun on 8/2/15.
+//  Copyright (c) 2015 Vicki Grospe. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var tipPercentage: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
