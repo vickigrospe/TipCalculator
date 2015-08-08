08630bf2e81ebf9a1a3396511ec3e15dd299c0ae
Vicki Chun <vickichun@vickis-mbp.attlocal.net>
2015-08-08 13:32:17 -0700

    Hide tip and total when Bill Amount is empty

--- MyTipster/ViewController.swift
+++ MyTipster/ViewController.swift
@@ -11,8 +11,9 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var tipValue: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
-    
+    @IBOutlet weak var totalValue: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     
     override func viewDidLoad() {
@@ -20,8 +21,6 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         billField.becomeFirstResponder()
-        tipLabel.text = "$0.00"
-        totalLabel.text = "$0.00"
     }
     
     
@@ -39,18 +38,33 @@ class ViewController: UIViewController {
         var tipPercentages = [0.18,0.2,0.25]
         var selectedTipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
         
-        var billAmount: Double = NSString(string: billField.text).doubleValue
+        if billField.text == "" {
+            hideElements(true)
+        }
+        else
+        {
+            var billAmount: Double = NSString(string: billField.text).doubleValue
+            
+            var tip = billAmount * selectedTipPercentage
+            var total = billAmount + tip
+            
+            tipValue.text = "$\(tip)"
+            totalValue.text = "$\(total)"
+            
+            tipValue.text = String(format: "$%.2f", tip)
+            totalValue.text = String(format: "$%.2f", total)
+            
+            hideElements(false)
+        }
         
-        var tip = billAmount * selectedTipPercentage
-        var total = billAmount + tip
-        
-        
-        tipLabel.text = "$\(tip)"
-        totalLabel.text = "$\(total)"
-        
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
-        
+    }
+    
+    func hideElements(hide: Bool) {
+        tipValue.hidden = hide
+        tipLabel.hidden = hide
+        totalValue.hidden = hide
+        totalLabel.hidden = hide
+        tipControl.hidden = hide
     }
     
     override func viewWillAppear(animated: Bool) {
